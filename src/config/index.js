6b74a8cd43d703ef7b00e5b18c86b57b6b65b39a
Muhammad Luthfi Wahid <mhmmadlthfi54@gmail.com>
2025-06-17 14:57:09 +0700

    merubah rest api src/config ke production

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,10 @@
-export const BASE_API_URL = "http://192.168.1.84:8000/api";
-export const BASE_STORAGE_URL = "http://192.168.1.84:8000/storage";
+// development
+// export const BASE_API_URL = "http://192.168.1.84:8000/api";
+// export const BASE_STORAGE_URL = "http://192.168.1.84:8000/storage";
+
+// production
+export const BASE_API_URL = "https://amalia-nurhanifah.unitech.my.id/api";
+export const BASE_STORAGE_URL =
+  "https://amalia-nurhanifah.unitech.my.id/storage";
+
 export const NO_WA_OWNER = "6285147038358";
